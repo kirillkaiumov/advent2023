d33401a7bd5bdde9094e331680e4a430ccdf9b4f
Kirill Kaiumov <k.kaiumov@catawiki.nl>
2023-12-09 12:31:00 +0100

    Day 9

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,7 +1,51 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  import Enum, except: [split: 2, reverse: 1]
+  import String, except: [at: 2, reverse: 1, length: 1]
+  import List, except: [to_integer: 1]
+
+  def part1(args) do
+    args
+    |> trim()
+    |> split("\n")
+    |> map(fn line ->
+      line = line |> split(" ") |> map(&to_integer/1)
+
+      do_part1(line, [])
+    end)
+    |> sum()
   end
 
-  def part2(_args) do
+  def do_part1(line, lasts) do
+    if all?(line, fn x -> x == 0 end) do
+      sum(lasts)
+    else
+      new_line = for i <- 1..(length(line) - 1), do: at(line, i) - at(line, i - 1)
+      lasts = [at(line, -1) | lasts]
+
+      do_part1(new_line, lasts)
+    end
+  end
+
+  def part2(args) do
+    args
+    |> trim()
+    |> split("\n")
+    |> map(fn line ->
+      line = line |> split(" ") |> map(&to_integer/1)
+
+      do_part2(line, [])
+    end)
+    |> sum()
+  end
+
+  def do_part2(line, lasts) do
+    if all?(line, fn x -> x == 0 end) do
+      reduce(lasts, 0, fn x, acc -> x - acc end)
+    else
+      new_line = for i <- 1..(length(line) - 1), do: at(line, i) - at(line, i - 1)
+      lasts = [at(line, 0) | lasts]
+
+      do_part2(new_line, lasts)
+    end
   end
 end
